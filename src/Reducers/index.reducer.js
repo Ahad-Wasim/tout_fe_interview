febb9891a6a2267dbed3a9d40af3631aa36a8ebc
Ahad Wasim <ahadwasim@gmail.com>
2017-08-20 06:03:18 -0700

    add content reducer

--- src/Reducers/index.reducer.js
+++ src/Reducers/index.reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { settings } from './settings.reducer.js';
+import { content } from './content.reducer.js';
 
 // constructs a new instance of the state
 const ApplicationState = combineReducers({
   settings,
+  content,
 });
 
 export { ApplicationState };
