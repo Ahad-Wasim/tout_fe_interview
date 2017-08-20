913fffef032da2804421eef7d4008fff17099e69
Ahad Wasim <ahadwasim@gmail.com>
2017-08-20 06:00:16 -0700

    changed setTimer constant in reducer file

--- src/Reducers/settings.reducer.js
+++ src/Reducers/settings.reducer.js
@@ -1,12 +1,12 @@
-import { settingModel } from './Initial-Models/settings.reducer.model.js';
-import { SET_TOTAL_TIMERS } from './Constants/settings.reducer.constants.js';
-import { customDeepClone } from './Utils/Helper/helper.js';
+import { settingModel } from './InitialModels/settings.model.js';
+import { SET_TIMER_COUNT } from '../Constants/constants';
+import { customDeepClone } from '../Utils/Helper/helper';
 
 // Returns a new Model
 const settings = (state=settingModel, action) => {
   switch (action.type) {
-    case SET_TOTAL_TIMERS:
-      return customDeepClone(state, ['settings', 'default'], { totalTimers: action.payload });
+    case SET_TIMER_COUNT:
+      return Object.assign({}, state, { timerCount: action.payload });
     default:
       return state;
   };
