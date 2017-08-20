fcc2020942a1a77d5e53f128208525d2c09a81c1
Ahad Wasim <ahadwasim@gmail.com>
2017-08-20 06:05:40 -0700

    updated models in content and settings

---
+++ src/Reducers/InitialModels/content.model.js
@@ -0,0 +1,5 @@
+const contentModel = {
+  headerValue: '',
+};
+
+export { contentModel };
---
+++ src/Reducers/InitialModels/settings.model.js
@@ -0,0 +1,26 @@
+const settingModel = {
+  default: {
+    timerCount: 6,
+    finishedClocks: [],
+    timePortrayal: 'months',
+    seconds: '00',
+    minutes: '00',
+    hours: '00',
+    days: '00',
+    months: '00',
+    years: '00',
+    notification: 'shake',
+  },
+  timerCount: null,
+  finishedClocks: null,
+  timePortrayal: null,
+  seconds: null,
+  minutes: null,
+  hours: null,
+  days: null,
+  months: null,
+  years: null,
+  notification: null,
+};
+
+export { settingModel };
