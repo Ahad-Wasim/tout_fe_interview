f306ce273272033c3cd272a23e8d4c49582f94f6
Ahad Wasim <ahadwasim@gmail.com>
2017-08-21 21:40:01 -0700

    removed configuration name

--- src/Actions/configurations.actions.js
+++
@@ -1,6 +0,0 @@
-import { SET_TIMER_COUNT } from '../Constants/constants.js'
-import { actionCreator } from '../Utils/Helper/helper';
-
-export const updateTimerCount = (value) => {
-  return actionCreator(SET_TIMER_COUNT, value);
-};
