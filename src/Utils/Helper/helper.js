495216fafe9fed16cc6fee263fed234ce26af2e7
Ahad Wasim <ahadwasim@gmail.com>
2017-08-21 21:39:06 -0700

    add start button visibitly check

--- src/Utils/Helper/helper.js
+++ src/Utils/Helper/helper.js
@@ -19,12 +19,28 @@ export const customDeepClone = (state, nestedProp, data) => {
   return clonedState;
 };
 
-// customDeepClone(state, ['settings', 'default'], { totalTimers: action.payload });
-
 export const actionCreator = (type, payload) => {
   return { type, payload };
 };
 
-export const determineDefaultOutput = (obj, prop) => {
-  return _.isNil(obj[prop]) ? obj.default[prop] : obj[prop];
+// Distinguish between regular values and default values
+export const determineValue = (val, def) => {
+  return _.isNil(val) ? def : val;
+};
+
+export const startButtonVisibility = (hours, minutes, seconds) => {
+
+  const finished = '00';
+
+  // Used nested if statments because it is easier to read versus putting
+  // all these conditionals on one line
+  if(_.isEqual(hours, finished)) {
+    if(_.isEqual(minutes, finished)) {
+      if(_.isEqual(seconds, finished)) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 };
