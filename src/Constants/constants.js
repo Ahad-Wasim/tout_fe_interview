d47e4c5cdded2dc2e157ce90ab778ef062d05a28
Ahad Wasim <ahadwasim@gmail.com>
2017-08-21 21:41:37 -0700

    add constants for individual timers

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -1,6 +1,12 @@
 /* CMS Content */
 export const FETCHED_CMS_DATA = 'FETCHED_CMS_DATA';
 
-
 /* Settings */
 export const SET_TIMER_COUNT = 'SET_TIMER_COUNT';
+
+/* Add Timer */
+export const EDIT_TIMER_ID = 'EDIT_TIMER_ID';
+export const EDIT_TIMER_CONFIGS = 'EDIT_TIMER_CONFIGS';
+export const UPDATE_TOTAL_TIMERS = 'UPDATE_TOTAL_TIMERS';
+export const REMOVE_TIMER = 'REMOVE_TIMER';
+export const SET_MODAL_VISIBILITY = 'SET_MODAL_VISIBILITY';
