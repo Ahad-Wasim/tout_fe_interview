4ea0b5245b17dfa204937382960daaa942157318
Ahad Wasim <ahadwasim@gmail.com>
2017-08-23 03:13:56 -0700

    no longer need compose for production

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 /* Components */
 import { Application } from './Components/ApplicationComponents/Application';
